19f0900d4d0649381082ccc3acb175f17fb04882
Jon Packard <jonpackard@users.noreply.github.com>
2020-12-14 00:47:41 -0500

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+mkdir -p /data/processed
+chmod 777 /data /data/processed
+cd /data
+while true; do
+    SLEEPCHECK=`ls /data | grep .URL`
+    while [ -z "$SLEEPCHECK" ]; do
+        echo Waiting 60 seconds before checking for new URLs...
+        sleep 60
+        SLEEPCHECK=`ls /data | grep .URL`
+    done
+    
+    for URLFILE in /data/*.URL; do
+        URL=`grep -i "URL=" "$URLFILE" | sed 's/URL=//g'`
+        echo "Attempting to download $URL"
+        youtube-dl --no-progress --write-thumbnail --exec "ffmpeg -i {} -vn -acodec copy {}.opus" "$URL"
+        mv "$URLFILE" processed/
+        chmod -R +rw /data
+    done
+done
